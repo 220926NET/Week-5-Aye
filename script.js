4c63af1b815d1e4ae5d51f17b57b345cf1c5af4c
ASOUDRY <soudryalex@gmail.com>
2022-10-26 13:21:25 -0400

    Misc changes

---
+++ script.js
@@ -0,0 +1,12 @@
+// Getting inputs from user, used to determine Challenge Rating
+// Inputs: Single player/Party && Levels
+
+// Use challenge rating to get an array of monsters that fall within that range from API (GET /api/monster with CR as query string parameter)
+
+// Randomly choose a monster from the list, start with only one monster per encounter but can expand upon it
+
+// Get a monster(s) that meets that CR (determined by levels input)
+
+// Display the monster's data
+
+let sayhi = (data) => () => console.log(data);
